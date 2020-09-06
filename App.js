4812553d12846dc8a3576be7dd23268875c26786
dnPC <denisagafonovwork.gmail.com>
2020-09-06 12:27:49 +0300

    init: add tab bar navigarion

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icons from "react-native-vector-icons/MaterialCommunityIcons";
+import WeatherPage from "./components/weather-page/weather-page";
+import TablePage from "./components/table-page/table-page";
+
+import "react-native-gesture-handler";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Weather"
+        tabBarOptions={{
+          activeTintColor: "#005fad",
+        }}
+      >
+        <Tab.Screen
+          name="Weather"
+          component={WeatherPage}
+          options={{
+            tabBarLabel: "Weather",
+            tabBarIcon: ({ color, size }) => (
+              <Icons name="apple-icloud" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="History"
+          component={TablePage}
+          options={{
+            tabBarLabel: "History",
+            tabBarIcon: ({ color, size }) => (
+              <Icons name="history" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
